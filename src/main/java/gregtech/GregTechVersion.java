4ebcee26a89e3de5edb8b1f954a0129ee821ba09
RONGMARIOLAPPY\Ethan <ethanmahmood@gmail.com>
2019-03-26 02:07:05 +0000

    Update version

--- src/main/java/gregtech/GregTechVersion.java
+++ src/main/java/gregtech/GregTechVersion.java
@@ -5,9 +5,9 @@ public final class GregTechVersion {
     //This number is incremented every major feature update, and reset every minecraft release
     public static final int MAJOR = 7;
     //This number is incremented every time the feature is added, or bug is fixed. resets every major version change
-    public static final int MINOR = 4;
+    public static final int MINOR = 5;
     //This number is incremented every build, and never reset.
-    public static final int BUILD = 13;
+    public static final int BUILD = 0;
 
     private GregTechVersion() {
     }
